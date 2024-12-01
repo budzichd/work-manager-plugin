17ed55843d1e81c5cc54ee07695af76ad8122ce9
Dawid Budzich <dawid.budzich@wolterskluwer.com>
2024-12-01 23:42:51 +0100

    switch to grid and add create new task component

---
+++ src/app/pages/tasks/new-task-dialog/new-task-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-new-task-dialog',
+	standalone: true,
+	imports: [],
+	templateUrl: './new-task-dialog.component.html',
+	styleUrl: './new-task-dialog.component.scss',
+})
+export class NewTaskDialogComponent {}
